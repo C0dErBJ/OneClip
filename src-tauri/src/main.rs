4fed116de936990351537b64e6ed9641be8bc824
cydz <youknowwhat@no.com>
2024-07-09 17:23:57 +0800

    更新logo以及日志

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -18,18 +18,35 @@ use tokio::{sync::broadcast, task::JoinSet};
 pub mod clip;
 pub mod config;
 pub mod connect;
-use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_appender::{non_blocking, rolling};
+use tracing_error::ErrorLayer;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, Registry};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     return format!("Hello, {}! You've been greeted from Rust!", name);
 }
+fn init_log() {
+    // 输出到控制台中
+    let formatting_layer = fmt::layer().pretty().with_writer(std::io::stderr);
+    // 输出到文件中
+    let file_appender = rolling::daily("logs", "app.log");
+    let (non_blocking_appender, _guard) = non_blocking(file_appender);
+    let file_layer = fmt::layer()
+        .with_ansi(false)
+        .with_writer(non_blocking_appender);
 
+    // 注册
+    Registry::default()
+        .with(ErrorLayer::default())
+        .with(formatting_layer)
+        .with(file_layer)
+        .init();
+}
 fn main() {
     //加载日志
-    tracing_subscriber::registry().with(fmt::layer()).init();
-    //加载日志
+    init_log();
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
     let hide = CustomMenuItem::new("hide".to_string(), "Hide");
     let tray_menu = SystemTrayMenu::new()
